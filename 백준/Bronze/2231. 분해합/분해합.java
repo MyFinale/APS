3f7b500a879d8eac0429c025dffbffd98b7061e4
Misanthropy <98503573+MyFinale@users.noreply.github.com>
2023-06-04 03:09:56 +0900

    [Bronze II] Title: 분해합, Time: 224 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int N, tmp, sum, ans = 0;
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        sc.close();
+
+        for (int i=1; i<N; i++) {
+
+            tmp = i;
+            sum = i;
+
+            while (tmp > 0) {
+                sum += tmp % 10;
+                tmp /= 10;
+            }
+
+            if (sum == N) {
+                ans = i;
+                break;
+            }
+
+        }
+
+        System.out.print(ans);
+
+    }
+}
