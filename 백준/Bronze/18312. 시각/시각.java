e04c7992b0ce43b76b082f2a25a5b5936a914e8f
Misanthropy <98503573+MyFinale@users.noreply.github.com>
2023-06-05 02:39:32 +0900

    [Bronze II] Title: 시각, Time: 212 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/18312. 시각/시각.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int N, K, h, m, s, ans = 0;
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        sc.close();
+
+        for (int i=0; i<(N + 1) * 60 * 60; i++) {
+            h = i / 3600;
+            m = i / 60 % 60;
+            s = i % 60;
+            ans += h / 10 == K || h % 10 == K || m / 10 == K || m % 10 == K || s / 10 == K || s % 10 == K ? 1 : 0;
+        }
+
+        System.out.print(ans);
+    }
+}
