35a0a0476292477e2cd8ccc56ca7fe4c8f0fb0f3
Misanthropy <98503573+MyFinale@users.noreply.github.com>
2023-06-06 04:33:00 +0900

    [Bronze II] Title: 피로도, Time: 216 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/22864. 피로도/피로도.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int A, B, C, M, pirodo = 0, work = 0;
+
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        M = sc.nextInt();
+        sc.close();
+
+        for (int i=0; i<24; i++) {
+            if (pirodo + A > M)
+                pirodo -= pirodo < C ? pirodo : C;
+            else {
+                work += B;
+                pirodo += A;
+            }
+        }
+
+        System.out.print(work);
+
+    }
+}
