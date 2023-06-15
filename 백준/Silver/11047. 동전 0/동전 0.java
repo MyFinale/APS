6ce4b24a6440fcccb00e8e9f1e9128f12ad79042
Misanthropy <98503573+MyFinale@users.noreply.github.com>
2023-06-16 02:17:58 +0900

    [Silver IV] Title: 동전 0, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int N, K, ans = 0;
+        int[] cost;
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        cost = new int[N];
+
+        for (int i=N-1; i>=0; i--)
+            cost[i] = sc.nextInt();
+        sc.close();
+
+        for (int value : cost)
+            if (value <= K) {
+                ans += K / value;
+                K %= value;
+            }
+
+        System.out.print(ans);
+
+    }
+}
