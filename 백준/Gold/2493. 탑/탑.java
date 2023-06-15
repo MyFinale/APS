8ef522297e35c4df4477a01cc0080c652f9a7229
Misanthropy <98503573+MyFinale@users.noreply.github.com>
2023-06-16 01:37:24 +0900

    [Gold V] Title: 탑, Time: 948 ms, Memory: 94480 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        Stack<Integer> index = new Stack<>();
+        Stack<Integer> height = new Stack<>();
+        height.push(0);
+        index.push(0);
+
+        for(int i=0; i<N; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            while(!height.isEmpty() && height.peek() < target) {
+                index.pop();
+                height.pop();
+            }
+            sb.append(height.isEmpty() ? 0 : index.peek()).append(" ");
+            height.push(target);
+            index.push(i + 1);
+        }
+        sb.setLength(sb.length() - 1);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
