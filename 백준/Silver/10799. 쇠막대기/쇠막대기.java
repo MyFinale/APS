1f73ff9cb6ce6520d3226deeb52b0e1d1c6eacd4
Misanthropy <98503573+MyFinale@users.noreply.github.com>
2023-06-16 01:34:36 +0900

    [Silver II] Title: 쇠막대기, Time: 340 ms, Memory: 22692 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        sc.close();
+
+        Stack<Character> stack = new Stack<>();
+        int ans = 0;
+        char last = '\0';
+
+        for(char ch : input.toCharArray()) {
+            if(ch == '(')
+                stack.push(ch);
+            else {
+                if(last != ch) {
+                    stack.pop();
+                    ans += stack.size();
+                }
+                else {
+                    stack.pop();
+                    ans += 1;
+                }
+            }
+            last = ch;
+        }
+
+        System.out.print(ans);
+    }
+}
