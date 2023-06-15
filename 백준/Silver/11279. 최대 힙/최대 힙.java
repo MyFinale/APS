fcd2114940aad93079365153f8ba76271126d322
Misanthropy <98503573+MyFinale@users.noreply.github.com>
2023-06-16 01:43:03 +0900

    [Silver II] Title: 최대 힙, Time: 1020 ms, Memory: 113516 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        while(N-- > 0) {
+            int target = sc.nextInt();
+            if(target == 0)
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+            else
+                pq.offer(target);
+        }
+
+        sc.close();
+        System.out.print(sb);
+
+    }
+
+}
