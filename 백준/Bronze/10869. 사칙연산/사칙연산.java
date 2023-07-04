4a4353fa04acefc60b43e70e34092e35d144d389
Misanthropy <98503573+MyFinale@users.noreply.github.com>
2023-07-04 20:58:40 +0900

    [Bronze V] Title: 사칙연산, Time: 136 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        bw.write(String.format("%d\n%d\n%d\n%d\n%d", A+B, A-B, A*B, A/B, A%B));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
